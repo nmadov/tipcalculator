cdc5859c93a50ad874211b41cd5b77225697bc30
nmadov <madame@gmail.com>
2015-03-16 20:51:15 -0400

    first commit tip calc

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-//add script here
+function tipCalculate(){
+	var totalBill = document.getElementById('bill').value;
+
+	var percBTns = document.getElementsByClassName('tip-btn');
+	
+	for (var i=0; i<percBTns.length; i++){
+		if (percBTns[i].checked){
+			var tipPerc = percBTns[i].value;
+		}
+	}
+	
+	var tipTotal = totalBill * tipPerc;
+
+	var tipBox = document.getElementById('tipamount');
+	tipBox.innerHTML = '$'+ tipTotal;
+
+	}
+
+	//copy submit button and create a function to reset the calculator
+	// check to false
+	// leave the value to zero
+	
