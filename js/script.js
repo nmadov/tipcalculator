37baff4e648b17b09c945ec31905143a25a045db
nmadov <madame@gmail.com>
2015-03-23 19:29:13 -0400

    reset commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function tipCalculate(){
 	var totalBill = document.getElementById('bill').value;
 
+	//console.log(isNaN(totalBill));
+
+
+
 	var percBTns = document.getElementsByClassName('tip-btn');
 	
 	for (var i=0; i<percBTns.length; i++){
@@ -9,12 +13,36 @@ function tipCalculate(){
 		}
 	}
 	
-	var tipTotal = totalBill * tipPerc;
 
-	var tipBox = document.getElementById('tipamount');
-	tipBox.innerHTML = '$'+ tipTotal;
-
+	if (!isNaN(totalBill)){
+		 var tipTotal = Math.floor(totalBill * tipPerc * 100)/100;
+		 var tipBox = document.getElementById('tipamount');
+		 tipBox.innerHTML = '$'+ tipTotal;
+	} else { 
+		alert('Please enter numbers only'); resetCalc();
 	}
+
+}
+
+	function resetCalc(){
+  document.getElementById('bill').value = '';
+
+  var percBtns = document.getElementsByClassName('tip-btn');
+  for(var i=0; i < percBtns.length; i++){
+    percBtns[i].checked = false;
+  }
+
+  var tipBox = document.getElementById('tipamount');
+  tipBox.innerHTML = '$0.00';
+}
+
+	// function resetCalculator(){
+	// 	var resetPage = document.getElementById('reset').onClick;
+
+	// 	for (var i=0; )
+
+
+	// }
 
 	//copy submit button and create a function to reset the calculator
 	// check to false
